cc2c36ddbe9193a3e26ec5773ac2b93210e64eb1
Savinykh Vladimir <awpm9so@gmail.com>
2024-09-23 00:33:56 +0300

    add readme

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,7 +21,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $exceptions->render(function (NotFoundHttpException $e, Request $request) {
             if ($request->is('api/*')) {
                 return response()->json([
-                    'message' => 'Record not found.'
+                    'message' => 'Запись не найдена.'
                 ], 404);
             }
         });
